8e1c5e94816f53b9379aa1d4d730c8374433bc5e
Alextech5 <alextech.eng@gmail.com>
2023-03-15 12:13:49 +0530

    Added Popup functionality and some minor changes

--- src/admin/SearchEmployeePage.java
+++ src/admin/SearchEmployeePage.java
@@ -6,7 +6,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.Icon;
-import javax.swing.JFrame;
 import javax.swing.border.EmptyBorder;
 
 import custom_classes.Theme;
